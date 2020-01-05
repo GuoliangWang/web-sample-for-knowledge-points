7408ce7670dd417752371220217c0f1ec4ad8de7
王国良 <wgl413@sina.com>
2020-01-05 14:13:11 +0800

    为掌握前端知识点写的小例子们

---
+++ testNodeExport.js
@@ -0,0 +1,10 @@
+/*
+MIT License http://www.opensource.org/licenses/mit-license.php
+Author Tobias Koppers @sokra
+*/
+"use strict";
+
+
+// module.exports = 1;
+module.exports.a = 1;
+console.log(exports)
